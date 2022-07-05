50f1be78f4d222bade2a7a01ff36b04c9b74698f
andrebellu <bellu.andrea@outlook.it>
2022-07-05 17:11:53 +0200

    i need add tickets

--- webapp/controller/Tickets.controller.js
+++ webapp/controller/Tickets.controller.js
@@ -35,37 +35,37 @@ sap.ui.define(
         var oList;
         var token;
         var defaultBody = {
-          IDRapportino: null,
-          IDUtente: null,
-          Utente: null,
-          IDCliente: null,
-          IDCommessa: null,
-          IDClienteSede: null,
-          IDProgetto: null,
-          IDProgettoAttivita: null,
-          IDTodoList: null,
-          Codice: null,
-          Descrizione: null,
-          Attivita: null,
-          Sede: "UF",
-          Destinazione: null,
-          Giorno: null,
-          Ore: null,
-          OreLavorate: null,
-          Km: null,
-          KmEuro: null,
-          Pedaggio: null,
-          Forfait: null,
-          Vitto: null,
-          Alloggio: null,
-          Noleggio: null,
-          Trasporti: null,
-          Varie: null,
-          Plus: null,
-          Fatturabile: null,
-          Bloccato: null,
-          SpeseVarie: null,
-          Docente: null,
+            IDRapportino: null,
+            IDUtente: null,
+            Utente: null,
+            IDCliente: null,
+            IDCommessa: null,
+            IDClienteSede: null,
+            IDProgetto: null,
+            IDProgettoAttivita: null,
+            IDTodoList: null,
+            Codice: null,
+            Descrizione: null,
+            Attivita: null,
+            Sede: "UF",
+            Destinazione: null,
+            Giorno: null,
+            Ore: null,
+            OreLavorate: null,
+            Km: null,
+            KmEuro: null,
+            Pedaggio: null,
+            Forfait: null,
+            Vitto: null,
+            Alloggio: null,
+            Noleggio: null,
+            Trasporti: null,
+            Varie: null,
+            Plus: null,
+            Fatturabile: null,
+            Bloccato: null,
+            SpeseVarie: null,
+            Docente: null,
         };
         return Controller.extend(
             "regesta.regestarapportini.controller.Tickets",
@@ -110,20 +110,30 @@ sap.ui.define(
                     var clientLength = oModel.getProperty("/clienti").length;
                     var IDOrder = oModel.getProperty(path + "/IDCommessa");
                     var orderLength = oModel.getProperty("/commesse").length;
-                    for(i = 0; i <= clientLength; i++)
-                    {
-                      if(IDClient == oModel.getProperty("/clienti/" + i + "/IDCliente")){
-                        code =  oModel.getProperty("/clienti/" + i + "/Codice");
-                        clientDescription = oModel.getProperty("/clienti/" + i + "/Descrizione");
-                        break;
-                      }
+                    for (i = 0; i <= clientLength; i++) {
+                        if (
+                            IDClient ==
+                            oModel.getProperty("/clienti/" + i + "/IDCliente")
+                        ) {
+                            code = oModel.getProperty(
+                                "/clienti/" + i + "/Codice"
+                            );
+                            clientDescription = oModel.getProperty(
+                                "/clienti/" + i + "/Descrizione"
+                            );
+                            break;
+                        }
                     }
-                    for(i = 0; i <= orderLength; i++)
-                    {
-                      if(IDOrder == oModel.getProperty("/commesse/" + i + "/IDCommessa")){
-                        orderDescription =  oModel.getProperty("/commesse/" + i + "/Descrizione");
-                        break;
-                      }
+                    for (i = 0; i <= orderLength; i++) {
+                        if (
+                            IDOrder ==
+                            oModel.getProperty("/commesse/" + i + "/IDCommessa")
+                        ) {
+                            orderDescription = oModel.getProperty(
+                                "/commesse/" + i + "/Descrizione"
+                            );
+                            break;
+                        }
                     }
                     var clientName = code + " - " + clientDescription;
                     oModel.setProperty(path + "/ClientName", clientName);
@@ -136,19 +146,28 @@ sap.ui.define(
                     insertDate = insertDate.toLocaleDateString("it-IT");
                     oModel.setProperty(path + "/DataInserimento", insertDate);
 
-                    var DataConsegnaRichiesta = context.getProperty("DataConsegnaRichiesta");
+                    var DataConsegnaRichiesta = context.getProperty(
+                        "DataConsegnaRichiesta"
+                    );
                     var timeStamp = DataConsegnaRichiesta.replace(/\D/g, "");
                     var DataConsegnaRichiesta = new Date(parseInt(timeStamp));
-                    DataConsegnaRichiesta = DataConsegnaRichiesta.toLocaleDateString("it-IT");
-                    oModel.setProperty(path + "/DataConsegna", DataConsegnaRichiesta);
+                    DataConsegnaRichiesta =
+                        DataConsegnaRichiesta.toLocaleDateString("it-IT");
+                    oModel.setProperty(
+                        path + "/DataConsegna",
+                        DataConsegnaRichiesta
+                    );
 
                     var UltimaModifica = context.getProperty("UltimaModifica");
                     var timeStamp = UltimaModifica.replace(/\D/g, "");
                     var UltimaModifica = new Date(parseInt(timeStamp));
                     UltimaModifica = UltimaModifica.toLocaleDateString("it-IT");
-                    var Edited = oModel.getProperty(path + "/UltimaModificaUtente")+ " - " + UltimaModifica
+                    var Edited =
+                        oModel.getProperty(path + "/UltimaModificaUtente") +
+                        " - " +
+                        UltimaModifica;
                     oModel.setProperty(path + "/Modifica", Edited);
-                    UltimaModifica
+                    UltimaModifica;
                     if (!this.pDialog) {
                         this.pDialog = this.loadFragment({
                             name: "regesta.regestarapportini.fragments.PopupTicket",
@@ -160,55 +179,87 @@ sap.ui.define(
                     });
                 },
                 onCancel: function (oEvent) {
-                  this.byId("popup").close();
+                    this.byId("popup").close();
                 },
-                APIclienti: function(){
-                  var request = {
-                    method : "POST",
-                    redirect : "follow",
-                  };
+                APIclienti: function () {
+                    var request = {
+                        method: "POST",
+                        redirect: "follow",
+                    };
 
-                  fetch("https://asstest.regestaitalia.it/api_v2/clienti?token="  + token + "&idCommessa=0&idCliente=0", request)
-                    .then((response) => response.text())
-                    .then((result) => this.handleClienti(result))
-                    .catch((error) => console.log("error", error));
+                    fetch(
+                        "https://asstest.regestaitalia.it/api_v2/clienti?token=" +
+                            token +
+                            "&idCommessa=0&idCliente=0",
+                        request
+                    )
+                        .then((response) => response.text())
+                        .then((result) => this.handleClienti(result))
+                        .catch((error) => console.log("error", error));
                 },
-                handleClienti: function(result){
-                  var oModel = this.getView().getModel();
-                  var clienti = JSON.parse(result);
-                  oModel.setProperty("/clienti", clienti);
+                handleClienti: function (result) {
+                    var oModel = this.getView().getModel();
+                    var clienti = JSON.parse(result);
+                    oModel.setProperty("/clienti", clienti);
                 },
-                APIcommesse: function(){
-                  var request = {
-                    method : "POST",
-                    redirect : "follow",
-                  };
-                  fetch("https://asstest.regestaitalia.it/api_v2/commesse?token=mF2rK0g%252bNh1xJnGB72RasA%253d%253d&idCommessa=0&idCliente=0", request)
-                    .then((response) => response.text())
-                    .then((result) => this.handleCommesse(result))
-                    .catch((error) => console.log("error", error));
+                APIcommesse: function () {
+                    var request = {
+                        method: "POST",
+                        redirect: "follow",
+                    };
+                    fetch(
+                        "https://asstest.regestaitalia.it/api_v2/commesse?token=mF2rK0g%252bNh1xJnGB72RasA%253d%253d&idCommessa=0&idCliente=0",
+                        request
+                    )
+                        .then((response) => response.text())
+                        .then((result) => this.handleCommesse(result))
+                        .catch((error) => console.log("error", error));
                 },
-                handleCommesse: function(result){
-                  var oModel = this.getView().getModel();
-                  var commesse = JSON.parse(result);
-                  oModel.setProperty("/commesse", commesse);
-                  console.log(clienti);
+                handleCommesse: function (result) {
+                    var oModel = this.getView().getModel();
+                    var commesse = JSON.parse(result);
+                    oModel.setProperty("/commesse", commesse);
+                    console.log(clienti);
                 },
-                APIticket: function(){
-                  var request = {
-                    method : "POST",
-                    redirect : "follow",
-                  };
-                  fetch("https://asstest.regestaitalia.it/api_v2/ticket?token="+ token + "&idTicket=0", request)
-                    .then((response) => response.text())
-                    .then((result) => this.handleTicket(result))
-                    .catch((error) => console.log("error", error));
+                APIticket: function () {
+                    var request = {
+                        method: "POST",
+                        redirect: "follow",
+                    };
+                    fetch(
+                        "https://asstest.regestaitalia.it/api_v2/ticket?token=" +
+                            token +
+                            "&idTicket=0",
+                        request
+                    )
+                        .then((response) => response.text())
+                        .then((result) => this.handleTicket(result))
+                        .catch((error) => console.log("error", error));
                 },
-                handleTicket: function(result){
-                  var oModel = this.getView().getModel();
-                  var ticket = JSON.parse(result);
-                  oModel.setProperty("/ticket", ticket);
-                }
+                handleTicket: function (result) {
+                    var oModel = this.getView().getModel();
+                    var ticket = JSON.parse(result);
+                    oModel.setProperty("/ticket", ticket);
+                },
+
+                Timer: function (oEvent) {
+                    if (check) {
+                        check = false;
+                        startTime = new Date().getTime();
+                        console.log(startTime);
+                    } else {
+                        endTime = new Date().getTime();
+                        console.log(endTime);
+                        timeDiff = endTime - startTime;
+                        console.log(timeDiff);
+                        var minutes = Math.floor(timeDiff / 60000);
+                        var seconds = ((timeDiff % 60000) / 1000).toFixed(0);
+                        console.log(
+                            minutes + ":" + (seconds < 10 ? "0" : "") + seconds
+                        );
+                        check = true;
+                    }
+                },
             }
         );
     }
